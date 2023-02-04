a2b753bb7a05080f119a8b87784ce780afa3bc0d
emrz98 <emrz98@gmail.com>
2023-02-04 11:52:35 -0600

    multiple consumers group added

--- kafka-core-consumer/kafka-core-consumer/src/main/java/com/course/kafka/kafkacoreconsumer/KafkaCoreConsumerApplication.java
+++ kafka-core-consumer/kafka-core-consumer/src/main/java/com/course/kafka/kafkacoreconsumer/KafkaCoreConsumerApplication.java
@@ -2,12 +2,15 @@ package com.course.kafka.kafkacoreconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class KafkaCoreConsumerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KafkaCoreConsumerApplication.class, args);
+			SpringApplication.run(KafkaCoreConsumerApplication.class, args);
+
 	}
 
 }
